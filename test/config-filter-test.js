3b2a0e364e5c5a985f4f66e2dc940170e65cde3d
Schabse Laks <Dev@SLaks.net>
2013-05-07 09:51:55 -0400

    Reorganize test groupings
    No code was changed

--- test/config-filter-test.js
+++ test/config-filter-test.js
@@ -15,77 +15,7 @@ describe('Hub', function () {
 	});
 
 	describe('.configure', function () {
-		describe('when minLevel is not set', function () {
-			it("should log everything", function () {
-				var messages = {};
-				var hub = logupHub.configureHub(module, [
-					{ type: "collector", messages: messages }
-				]);
-				var logger = logupEmitter.createLogger(module);
-				expect(logger.minLevel).to.be(0);
-
-				logger.trace("...");
-				logger.info("Hi!");
-				logger.warn("Hmm?");
-				logger.error("Bye!");
-
-				expect(messages.trace).to.have.property('length', 1);
-				expect(messages.info).to.have.property('length', 1);
-				expect(messages.warn).to.have.property('length', 1);
-				expect(messages.error).to.have.property('length', 1);
-			});
-			it("should inherit minLevel from parent group", function () {
-				var messages = {};
-				var hub = logupHub.configureHub(module, [
-					{
-						type: "filter", minLevel: "warn",
-						layers: [{ type: "collector", messages: messages }]
-					}
-				]);
-				var logger = logupEmitter.createLogger(module);
-				expect(logger.minLevel).to.be(logupEmitter.levels.values.warn);
-
-				logger.trace("...");
-				logger.info("Hi!");
-				logger.warn("Hmm?");
-				logger.error("Bye!");
-
-				expect(messages.trace).to.be.empty();
-				expect(messages.info).to.be.empty();
-				expect(messages.warn).to.have.property('length', 1);
-				expect(messages.error).to.have.property('length', 1);
-			});
-			it("should inherit minLevel from parent group in the presence of other loggers", function () {
-				var messages1 = {}, messages2 = {};
-				var hub = logupHub.configureHub(module, [
-					{
-						type: "filter", minLevel: "warn",
-						layers: [
-							{ type: "collector", messages: messages1 },
-							{ type: "collector", minLevel: "error", messages: messages2 },
-						]
-					}
-				]);
-				var logger = logupEmitter.createLogger(module);
-				expect(logger.minLevel).to.be(logupEmitter.levels.values.warn);
-
-				logger.trace("...");
-				logger.info("Hi!");
-				logger.warn("Hmm?");
-				logger.error("Bye!");
-
-				expect(messages1.trace).to.be.empty();
-				expect(messages1.info).to.be.empty();
-				expect(messages1.warn).to.have.property('length', 1);
-				expect(messages1.error).to.have.property('length', 1);
-
-				expect(messages2.trace).to.be.empty();
-				expect(messages2.info).to.be.empty();
-				expect(messages2.warn).to.be.empty();
-				expect(messages2.error).to.have.property('length', 1);
-			});
-		});
-		describe('when minLevel is set', function () {
+		describe('minLevel', function () {
 			it("should not log below that level", function () {
 				var messages = {};
 				var hub = logupHub.configureHub(module, [
@@ -105,32 +35,102 @@ describe('Hub', function () {
 				expect(messages.warn).to.have.property('length', 1);
 				expect(messages.error).to.have.property('length', 1);
 			});
-		});
+			describe('when minLevel is not set', function () {
+				it("should log everything", function () {
+					var messages = {};
+					var hub = logupHub.configureHub(module, [
+						{ type: "collector", messages: messages }
+					]);
+					var logger = logupEmitter.createLogger(module);
+					expect(logger.minLevel).to.be(0);
 
-		describe('when minLevel is set differently', function () {
-			it("should respect each setting", function () {
-				var messages1 = {}, messages2 = {};
-				var hub = logupHub.configureHub(module, [
-					{ type: "collector", messages: messages1, minLevel: "warn" },
-					{ type: "collector", messages: messages2, minLevel: "info" }
-				]);
-				var logger = logupEmitter.createLogger(module);
+					logger.trace("...");
+					logger.info("Hi!");
+					logger.warn("Hmm?");
+					logger.error("Bye!");
 
-				expect(logger.minLevel).to.be(logupEmitter.levels.values.info);
+					expect(messages.trace).to.have.property('length', 1);
+					expect(messages.info).to.have.property('length', 1);
+					expect(messages.warn).to.have.property('length', 1);
+					expect(messages.error).to.have.property('length', 1);
+				});
+				it("should inherit minLevel from parent group", function () {
+					var messages = {};
+					var hub = logupHub.configureHub(module, [
+						{
+							type: "filter", minLevel: "warn",
+							layers: [{ type: "collector", messages: messages }]
+						}
+					]);
+					var logger = logupEmitter.createLogger(module);
+					expect(logger.minLevel).to.be(logupEmitter.levels.values.warn);
 
-				logger.trace("...");
-				logger.info("Hi!");
-				logger.warn("Hmm?");
-				logger.error("Bye!");
+					logger.trace("...");
+					logger.info("Hi!");
+					logger.warn("Hmm?");
+					logger.error("Bye!");
 
-				expect(messages1.trace).to.be.empty();
-				expect(messages1.info).to.be.empty();
-				expect(messages1.warn).to.have.property('length', 1);
-				expect(messages1.error).to.have.property('length', 1);
+					expect(messages.trace).to.be.empty();
+					expect(messages.info).to.be.empty();
+					expect(messages.warn).to.have.property('length', 1);
+					expect(messages.error).to.have.property('length', 1);
+				});
+				it("should inherit minLevel from parent group in the presence of other loggers", function () {
+					var messages1 = {}, messages2 = {};
+					var hub = logupHub.configureHub(module, [
+						{
+							type: "filter", minLevel: "warn",
+							layers: [
+								{ type: "collector", messages: messages1 },
+								{ type: "collector", minLevel: "error", messages: messages2 },
+							]
+						}
+					]);
+					var logger = logupEmitter.createLogger(module);
+					expect(logger.minLevel).to.be(logupEmitter.levels.values.warn);
 
-				expect(messages2.trace).to.be.empty();
-				expect(messages2.info).to.have.property('length', 1);
-				expect(messages2.warn).to.have.property('length', 1);
+					logger.trace("...");
+					logger.info("Hi!");
+					logger.warn("Hmm?");
+					logger.error("Bye!");
+
+					expect(messages1.trace).to.be.empty();
+					expect(messages1.info).to.be.empty();
+					expect(messages1.warn).to.have.property('length', 1);
+					expect(messages1.error).to.have.property('length', 1);
+
+					expect(messages2.trace).to.be.empty();
+					expect(messages2.info).to.be.empty();
+					expect(messages2.warn).to.be.empty();
+					expect(messages2.error).to.have.property('length', 1);
+				});
+			});
+
+			describe('when a filter group has transports with different levels', function () {
+				it("should respect each setting", function () {
+					var messages1 = {}, messages2 = {};
+					var hub = logupHub.configureHub(module, [
+						{ type: "collector", messages: messages1, minLevel: "warn" },
+						{ type: "collector", messages: messages2, minLevel: "info" }
+					]);
+					var logger = logupEmitter.createLogger(module);
+
+					expect(logger.minLevel).to.be(logupEmitter.levels.values.info);
+
+					logger.trace("...");
+					logger.info("Hi!");
+					logger.warn("Hmm?");
+					logger.error("Bye!");
+
+					expect(messages1.trace).to.be.empty();
+					expect(messages1.info).to.be.empty();
+					expect(messages1.warn).to.have.property('length', 1);
+					expect(messages1.error).to.have.property('length', 1);
+
+					expect(messages2.trace).to.be.empty();
+					expect(messages2.info).to.have.property('length', 1);
+					expect(messages2.warn).to.have.property('length', 1);
+				});
 			});
 		});
 	});
